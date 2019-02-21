15ead4cd3d685ee6a49ffb2e0309314ceeecfd79
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-02-21 18:46:44 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,272 @@
+Config              = {}
+Config.DrawDistance = 100.0
+Config.MaxDelivery	= 24
+Config.TruckPrice	= 0
+Config.TruckRental  = 5000
+Config.Locale = 'en'
+
+Config.Trucks = {
+	"phantom3"
+}
+
+Config.Cloakroom = {
+	CloakRoom = {
+			Pos   = {x = 580.86, y = -2295.28, z = 5.15},
+			Size  = {x = 2.6, y = 2.6, z = 1.0},
+			Color = {r = 0, g = 255, b = 0},
+			Type  = 27
+		},
+}
+
+Config.Zones = {
+	VehicleSpawner = {
+			Pos   = {x = 581.18, y = -2291.9499999999, z = 5.13},
+			Size  = {x = 2.6, y = 2.6, z = 1.0},
+			Color = {r = 0, g = 0, b = 255},
+			Type  = 27
+		},
+
+	VehicleSpawnPoint = {
+			Pos   = {x = 588.24, y = -2229.99, z = 5.01},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Type  = -1
+		},
+}
+
+Config.Livraison = {
+-------------------------------------------Los Santos
+	-- 916 Route 68 (Xero Station)
+	Delivery1LS = {
+			Pos   = {x = 43.780000000001, y = 2776.2000000001, z = 56.89},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 920 Route 68 (Garage General Store)
+	Delivery2LS = {
+			Pos   = {x = 262.87, y = 2611.58, z = 43.91},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 937 Route 68 (Cafe)
+	Delivery3LS = {
+			Pos   = {x = 1038.28, y = 2681.26, z = 38.38},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 941 Route 68
+	Delivery4LS = {
+			Pos   = {x = 1210.03, y = 2662.4, z = 36.85},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 960 Senora Way
+	Delivery5LS = {
+			Pos   = {x = 2536.8, y = 2593.93, z = 37.02},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 957 Route 13
+	Delivery6LS = {
+			Pos   = {x = 2682.28, y = 3262.47, z = 54.26},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 1036 Alhambra Drive
+	Delivery7LS = {
+			Pos   = {x = 2008.93, y = 3771.69, z = 31.21},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 2013 Grapseed Maint Street
+	Delivery8LS = {
+			Pos   = {x = 1698.47, y = 4915.12, z = 41.12},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 3030 Great Ocean Highway
+	Delivery9LS = {
+			Pos   = {x = 1699.73, y = 6413.77, z = 31.87},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 3025 Route 1
+	Delivery10LS = {
+			Pos   = {x = 201.0, y = 6618.47, z = 30.78},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 3016 Paleto Boulevard
+	Delivery11LS = {
+			Pos   = {x = -96.350000000001, y = 6421.92, z = 30.52},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 1001 Route 68
+	Delivery12LS = {
+			Pos   = {x = -2554.45, y = 2322.76, z = 32.07},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 817 North Rockford Drive
+	Delivery13LS = {
+			Pos   = {x = -1789.44, y = 811.3, z = 137.57},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 644 North Rockford Drive
+	Delivery14LS = {
+			Pos   = {x = -1409.69, y = -277.75, z = 45.44},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 602 Great Ocean Highway
+	Delivery15LS = {
+			Pos   = {x = -2092.68, y = -322.11, z = 12.04},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 366 Lindsay Circus
+	Delivery16LS = {
+			Pos   = {x = -728.15, y = -933.89, z = 18.08},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 375 Dutch London Street
+	Delivery17LS = {
+			Pos   = {x = -523.69, y = -1204.03, z = 17.3},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 2.9, y = 2.9, z = 3.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 120 Grove Street
+	Delivery18LS = {
+			Pos   = {x = -81.48, y = -1756.26, z = 28.66},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 139 Strawberry Avenue
+	Delivery19LS = {
+			Pos   = {x = 269.12, y = -1258.93, z = 28.28},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 230 Vespucci Boulevard
+	Delivery20LS = {
+			Pos   = {x = 823.15, y = -1028.52, z = 25.33},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 177 El Rancho Boulevard
+	Delivery21LS = {
+			Pos   = {x = 1198.15, y = -1391.17, z = 34.25},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 411 Mirror Park Boulevard
+	Delivery22LS = {
+			Pos   = {x = 1171.04, y = -316.79, z = 68.19},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 596 Clinton Avenue
+	Delivery23LS = {
+			Pos   = {x = 616.81, y = 268.67, z = 102.09},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	-- 402 Route 15
+	Delivery24LS = {
+			Pos   = {x = 2593.58, y = 360.26, z = 107.5},
+			Color = {r = 204, g = 204, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 5.0},
+			Color = {r = 204, g = 204, b = 0},
+			Type  = 27,
+			Paye = 1000
+		},
+	
+	RetourCamion = {
+			Pos   = {x = 574.19, y = -2283.3500000001, z = 5.02},
+			Color = {r = 255, g = 0, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 3.0},
+			Color = {r = 255, g = 0, b = 0},
+			Type  = 27,
+			Paye = 0
+		},
+	
+	AnnulerMission = {
+			Pos   = {x = 580.51, y = -2241.74, z = 4.99},
+			Color = {r = 255, g = 0, b = 0},
+			Size  = {x = 5.0, y = 5.0, z = 3.0},
+			Color = {r = 255, g = 0, b = 0},
+			Type  = 27,
+			Paye = 0
+		},
+}
